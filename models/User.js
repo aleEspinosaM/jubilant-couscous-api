227dccec9473a5e13a5824ca917a2609846deb68
Alejandro Espinosa <alex.espinosadelosmonteros@gmail.com>
2017-05-09 21:34:06 -0500

    login routes for user

--- models/User.js
+++ models/User.js
@@ -1,49 +1,35 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
-const crypto = require('crypto');
-const jwt = require('jsonwebtoken');
-const secret = require('../config').secret;
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+var secret = require('../config').secret;
 
-const UserSchema = new mongoose.Schema({
-    username: {
-        type: String, 
-        lowercase: true, 
-        required: [true, "can't be blank"], 
-        unique: true,
-        match: [/^[a-zA-Z0-9]+$/, 'is invalid'], 
-        index: true
-    },
-    email: {
-        type: String, 
-        lowercase: true, 
-        required: [true, "can't be blank"], 
-        unique: true,
-        match: [/\S+@\S+\.\S+/, 'is invalid'],
-        index: true //create index to optimize queries
-    },
-    bio: 'String',
-    image: 'String',
-    hash: 'String',
-    salt: 'String'
-},{
-    timestamps: true
-});
+var UserSchema = new mongoose.Schema({
+  username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
+  email: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
+  bio: String,
+  image: String,
+  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }],
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  hash: String,
+  salt: String
+}, {timestamps: true});
 
-UserSchema.plugin(uniqueValidator, { message: 'is already taken' })
+UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
 
-UserSchema.methods.setPassword = (password) => {
-    this.salt = crypto.randomBytes(16).toString('hex');
-    this.hash = crypto.pbkdf2Sync(pasword, this.salt, 1000, 512, 'sha512').toString('hex');
-}
+UserSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+  return this.hash === hash;
+};
 
-UserSchema.methods.validPassword = (password) => {
-    let hash = crypto.pbkdf2Sync(password, this.salt, 1000, 521, 'sha512').toString('hex');
-    return this.hash === hash
-}
+UserSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+};
 
-UserSchema.methods.generateJWT = () => {
-  const today = new Date();
-  let exp = new Date(today);
+UserSchema.methods.generateJWT = function() {
+  var today = new Date();
+  var exp = new Date(today);
   exp.setDate(today.getDate() + 60);
 
   return jwt.sign({
@@ -54,13 +40,11 @@ UserSchema.methods.generateJWT = () => {
 };
 
 UserSchema.methods.toAuthJSON = function(){
-    return {
-        username: this.username,
-        email: this.email,
-        token: this.generateJWT()
+  return {
+    username: this.username,
+    email: this.email,
+    token: this.generateJWT()
   };
 };
 
-
 mongoose.model('User', UserSchema);
-
