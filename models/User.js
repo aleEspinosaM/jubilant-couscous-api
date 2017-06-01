9d2fe1d07050564998108904be41bcbabd4c7a0b
Alejandro Espinosa <alex.espinosadelosmonteros@gmail.com>
2017-05-31 20:50:07 -0500

    Following and unfollowing functionality

--- models/User.js
+++ models/User.js
@@ -52,8 +52,27 @@ UserSchema.methods.toProfileJSONFor = function(user) {
     username: this.username,
     bio: this.bio,
     image: this.image || 'https://static.productionready.io/images/smiley-cyrus.jpg',
-    following:  false  // hardcoded for now 
+    following: user ? user.isFollowing(this._id) : false
   };
+};
+
+UserSchema.methods.follow = function(id){
+  if(this.favorites.indexOf(id) === -1){
+    this.following.push(id);
+  }
+
+  return this.save();
+};
+
+UserSchema.methods.unfollow = function(id){
+  this.following.remove(id);
+  return this.save();
+};
+
+UserSchema.methods.isFollowing = function(id){
+  return this.following.some(function(followId){
+    return followId.toString() === id.toString();
+  });
 };
 
 UserSchema.methods.favorite = function(id){
--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -26,5 +26,31 @@ router.get('/:username', auth.optional, function(req, res, next){
   }
 });
 
+// follow a user
+router.post('/:username/follow', auth.required, function(req, res, next){
+  var profileId = req.profile._id;
+
+  User.findById(req.payload.id).then(function(user){
+    if (!user) { return res.sendStatus(401); }
+
+    return user.follow(profileId).then(function(){
+      return res.json({profile: req.profile.toProfileJSONFor(user)});
+    });
+  }).catch(next);
+});
+
+// unfollow a user
+
+router.delete('/:username/follow', auth.required, function(req, res, next){
+  var profileId = req.profile._id;
+
+  User.findById(req.payload.id).then(function(user){
+    if (!user) { return res.sendStatus(401); }
+
+    return user.unfollow(profileId).then(function(){
+      return res.json({profile: req.profile.toProfileJSONFor(user)});
+    });
+  }).catch(next);
+});
 
 module.exports = router;
